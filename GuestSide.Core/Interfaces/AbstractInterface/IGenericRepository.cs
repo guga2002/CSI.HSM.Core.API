067fe3715d519579fd15a83ef66d9df07095e207
aapkh <aapkh@GUGAAPKH>
2024-08-03 20:24:59 +0400

    Generic repository

---
+++ GuestSide.Core/Interfaces/AbstractInterface/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace GuestSide.Core.Interfaces.AbstractInterface
+{
+
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(object id);
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task<bool> AddAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(object id);
+        Task<bool> Delete(T entityToDelete);
+    }
+}
---
+++ GuestSide.Infrastructure/Repositories/AbstractRepository/GenericRepository.cs
@@ -0,0 +1,101 @@
+﻿using GuestSide.Core.Interfaces.AbstractInterface;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace GuestSide.Infrastructure.Repositories.AbstractRepository
+{
+    public abstract class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly DbContext Context;
+        protected readonly DbSet<T> DbSet;
+
+        public GenericRepository(DbContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+            DbSet = context.Set<T>();
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public virtual async Task<T> GetByIdAsync(object id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            var entity = await DbSet.FindAsync(id);
+            if (entity == null)
+            {
+                throw new KeyNotFoundException($"Entity with id {id} not found.");
+            }
+
+            return entity;
+        }
+
+        public virtual async Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await DbSet.Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task<bool> AddAsync(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            await DbSet.AddAsync(entity);
+            return await Context.SaveChangesAsync() > 0;
+        }
+
+        public virtual async Task<bool> UpdateAsync(T entity)
+        {
+            if (entity == null)
+            {
+                throw new ArgumentNullException(nameof(entity));
+            }
+
+            DbSet.Attach(entity);
+            Context.Entry(entity).State = EntityState.Modified;
+            return await Context.SaveChangesAsync() > 0;
+        }
+
+        public virtual async Task<bool> DeleteAsync(object id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+
+            var entityToDelete = await DbSet.FindAsync(id);
+            if (entityToDelete == null)
+            {
+                throw new KeyNotFoundException($"Entity with id {id} not found.");
+            }
+
+            await Delete(entityToDelete);
+            return await Context.SaveChangesAsync() > 0;
+        }
+
+        public async virtual Task<bool> Delete(T entityToDelete)
+        {
+            if (entityToDelete == null)
+            {
+                throw new ArgumentNullException(nameof(entityToDelete));
+            }
+
+            if (Context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                DbSet.Attach(entityToDelete);
+            }
+
+            DbSet.Remove(entityToDelete);
+
+            return await Context.SaveChangesAsync() > 0;
+        }
+    }
+}
