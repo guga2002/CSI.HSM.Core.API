7168776053156240d5c7c5c43d95beb2c6fb6b33
Raisa123 <raya.badalova@gmail.com>
2024-09-14 16:03:27 +0400

    Add CustomEcxeptions

---
+++ GuestSide.Application/CustomExceptions/OperationFailedException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace GuestSide.Application.CustomExceptions
+{
+    public class OperationFailedException : Exception
+    {
+        public OperationFailedException()
+        {
+        }
+
+        public OperationFailedException(string? message) : base(message)
+        {
+        }
+
+        public OperationFailedException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected OperationFailedException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ GuestSide.Application/CustomExceptions/ServiceUnavailableException.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace GuestSide.Application.CustomExceptions
+{
+    public class ServiceUnavailableException : Exception
+    {
+        public ServiceUnavailableException()
+        {
+        }
+
+        public ServiceUnavailableException(string? message) : base(message)
+        {
+        }
+
+        public ServiceUnavailableException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ServiceUnavailableException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
