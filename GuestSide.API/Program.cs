31437170f3f12e2d02cd616716f45fcbf810768d
aapkh <aapkh@DESKTOP-JT3FIU7>
2024-10-06 22:41:05 +0400

    Update

--- GuestSide.API/Program.cs
+++ GuestSide.API/Program.cs
@@ -10,6 +10,13 @@ using GuestSide.Application.Services.Staff.Category;
 using GuestSide.Application.Services.Task.Category;
 using GuestSide.Application.Services.Task.Status;
 using GuestSide.Application.Services.Staff.Staf;
+using GuestSide.API.CustomMiddlwares;
+using GuestSide.Application.Services.Guest;
+using GuestSide.Application.Services.Item.DI;
+using GuestSide.Application.Services.LogService;
+using GuestSide.Application.Services.Notification.DI;
+using GuestSide.Application.Services.Room.DI;
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
@@ -22,7 +29,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<GuestSideDb>(
     str =>
     {
-        str.UseSqlServer(builder.Configuration.GetConnectionString("CSIGuest"));
+        str.UseSqlServer(builder.Configuration.GetConnectionString("CSICOnnect"));
     }
 );
 
@@ -38,6 +45,17 @@ builder.Services.InjectStaffs();
 builder.Services.InjectTaskCategory();
 builder.Services.InjectTasks();
 builder.Services.InjectTaskStatus();
+builder.Services.InjectGuest();
+builder.Services.InjectCart();
+builder.Services.InjectItemCategory();
+builder.Services.InjectItem();
+builder.Services.InjectLog();
+builder.Services.InjectGuestNotification();
+builder.Services.InjectNotification();
+builder.Services.InjectStaffNotification();
+builder.Services.InjectQrCode();
+builder.Services.InjectRoomCategory();
+builder.Services.InjectRoom();
 
 builder.Services.AddAutoMapper(typeof(GuestSide.Application.Mapper.AutoMapper));
 
@@ -55,7 +73,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseMiddleware<CustomMiddlwares>();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
--- GuestSide.Core/Migrations/20241006181736_MGRT.Designer.cs
+++ GuestSide.Core/Migrations/20241006181736_MGRT.Designer.cs
@@ -4,6 +4,7 @@ using GuestSide.Core.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GuestSide.Core.Migrations
 {
     [DbContext(typeof(GuestSideDb))]
-    partial class GuestSideDbModelSnapshot : ModelSnapshot
+    [Migration("20241006181736_MGRT")]
+    partial class MGRT
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
