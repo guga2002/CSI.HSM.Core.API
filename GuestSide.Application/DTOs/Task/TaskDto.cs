79bbab58f155ec6f3d1c95ac5320199f79c35afb
aapkh <aapkh@DESKTOP-JT3FIU7>
2024-10-13 23:08:50 +0400

    Update

--- GuestSide.Application/DTOs/Task/TaskDto.cs
+++ GuestSide.Application/DTOs/Task/TaskDto.cs
@@ -1,8 +1,4 @@
-﻿using GuestSide.Core.Entities.Task;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
-
-namespace GuestSide.Application.DTOs.Task
+﻿namespace GuestSide.Application.DTOs.Task
 {
     public class TaskDto
     {
@@ -10,13 +6,9 @@ namespace GuestSide.Application.DTOs.Task
 
         public required string Description { get; set; }
 
-        [DataType(DataType.Date)]
         public DateTime CreatedDate { get; set; }
 
-        [ForeignKey(nameof(Category))]
         public long CategoryId { get; set; }
-
-        public TaskCategory Category { get; set; }
 
         public long CartId { get; set; }
 
