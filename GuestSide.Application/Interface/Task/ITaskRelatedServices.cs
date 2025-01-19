db4796594114c7ab08dd3c00fba5363ddccac65a
TabaxmelaPC <TabaxmelaPC@DESKTOP-J7JC3FM>
2025-01-19 14:52:30 +0400

    Update

---
+++ GuestSide.Application/Interface/Task/ITaskRelatedServices.cs
@@ -0,0 +1,18 @@
+﻿using Core.Application.DTOs.Response.Item;
+using GuestSide.Application.DTOs.Request.Task;
+using GuestSide.Application.DTOs.Response.Item;
+using GuestSide.Application.DTOs.Response.Task;
+using GuestSide.Application.Interface;
+using GuestSide.Core.Entities.Task;
+
+namespace Core.Application.Interface.Task;
+
+public interface ITaskRelatedServices : IService<TaskDto, TaskResponseDto, long, Tasks>,IService<TaskCategoryDto, TaskCategoryResponseDto, long, TaskCategory>, IService<TaskStatusDto, TaskStatusResponseDto, long, TaskStatus>,
+    IService<TaskStatusDto, TaskStatusResponseDto, long, TasksStatus>
+{
+    Task<TaskCategoryResponseDto>GetTaskCategoryByTaskId(long TaskId, CancellationToken cancellationToken = default);
+
+    Task<OrderableItemResponseDto> GetOrderableItemByTaskId(long TaskId, CancellationToken cancellationToken = default);
+
+    Task<CartResponseDto> GetCartByTaskId(long TaskId, CancellationToken cancellationToken = default);
+}
--- GuestSide.Application/Interface/Task/Task/ITaskService.cs
+++ GuestSide.Application/Interface/Task/Task/ITaskService.cs
@@ -1,11 +1,15 @@
 ﻿using GuestSide.Application.DTOs.Request.Task;
+using GuestSide.Application.DTOs.Response.Item;
 using GuestSide.Application.DTOs.Response.Task;
+using GuestSide.Core.Entities.Item;
 using GuestSide.Core.Entities.Task;
 
 namespace GuestSide.Application.Interface.Task.Task
 {
     public interface ITaskService : IService<TaskDto,TaskResponseDto, long, Tasks>
     {
-        Task<TaskResponseDto> GetTaskbycartId(long CartId, CancellationToken cancellationToken = default);
+        Task<TaskCategoryResponseDto> GetTaskCategoryByTaskId(long TaskID, CancellationToken cancellationToken = default);
+
+        Task<CartResponseDto> GetCardByTaskId(long TaskID, CancellationToken cancellationToken = default);
     }
 }
