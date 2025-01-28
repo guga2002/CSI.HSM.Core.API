5727d03a8eaff811cbcc5e839e3629ae3697fe6a
Guga's PC <Guga's PC@DESKTOP-2DB9EPL>
2025-01-28 09:18:22 -0800

    Update

---
+++ GuestSide.Core/Migrations/20250127175037_kjdf.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Core.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class kjdf : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Quantity",
+                schema: "CSI",
+                table: "Tasks");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Quantity",
+                schema: "CSI",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
