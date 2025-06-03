4b6eee956f5b534f6746598bb45bbd5c6b0fc1a7
Raisa Badalova <raisa.badalova132@ens.tsu.ge>
2025-06-03 11:45:29 +0400

    Update

--- GuestSide.API/Program.cs
+++ GuestSide.API/Program.cs
@@ -66,7 +66,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontendLocalhost", policy =>
     {
-        policy.WithOrigins("http://localhost:4200")
+        policy.WithOrigins("http://localhost:4200", "https://localhost:4200")
               .AllowAnyHeader()
               .AllowAnyMethod()
               .AllowCredentials();
